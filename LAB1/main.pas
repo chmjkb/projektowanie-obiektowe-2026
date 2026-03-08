712d6536db502e6741fd3e81cf3df0fc4f367259
chmjkb <jakubchmura1607@gmail.com>
2026-03-08 20:50:16 +0100

    3.5 Procedura do sortowania liczb

--- LAB1/main.pas
+++ LAB1/main.pas
@@ -19,4 +19,12 @@ begin
   for I := 0 to COUNT - 1 do
     Write(Arr[I], ' ');
   WriteLn;
+
+  WriteLn;
+  WriteLn('After sorting:');
+  SortNumbers(Arr);
+
+  for I := 0 to COUNT - 1 do
+    Write(Arr[I], ' ');
+  WriteLn;
 end.
--- LAB1/numbers.pas
+++ LAB1/numbers.pas
@@ -5,6 +5,7 @@ unit Numbers;
 interface
 
 procedure GenerateRandom(var Arr: array of Integer);
+procedure SortNumbers(var Arr: array of Integer);
 
 implementation
 
@@ -15,6 +16,20 @@ begin
   Randomize;
   for I := 0 to High(Arr) do
     Arr[I] := Random(101);
+end;
+
+procedure SortNumbers(var Arr: array of Integer);
+var
+  I, J, Temp: Integer;
+begin
+  for I := 0 to High(Arr) - 1 do
+    for J := 0 to High(Arr) - I - 1 do
+      if Arr[J] > Arr[J + 1] then
+      begin
+        Temp := Arr[J];
+        Arr[J] := Arr[J + 1];
+        Arr[J + 1] := Temp;
+      end;
 end;
 
 end.
